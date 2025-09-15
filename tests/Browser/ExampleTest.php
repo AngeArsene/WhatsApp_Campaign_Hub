53548c681fbb9c0ed7555161689f5d0a2d185940
Ange Arsene <nkenmandenga@gmail.com>
2025-09-15 09:22:54 +0100

    fix: update composer packages, install dusk chrome driver and setup first browser test.

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -13,7 +13,9 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/contacts');
+            $browser->visit('/contacts')
+                    ->assertSee('Contacts')
+                    ->assertSee('Manage your customer contact list');
         });
     }
 }
